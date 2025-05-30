a4455171d63961141605e589b8f50707b7674570
smRid <omorsk911@gmail.com>
2025-05-31 01:12:48 +0600

    database setup with neon postgresql

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
 import express from 'express';
 import dotenv from "dotenv"
+import { sql } from './config/db.js'; 
 
 dotenv.config();
 
 const app = express();
+
+async function initDB() {
+    try {
+       await sql`CREATE TABLE IF NOT EXISTS transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10, 2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`
+
+         console.log('Database initialized successfully');
+    } catch (error) {
+        console.log('Error initializing database:', error);
+        process.exit(1); //status code 1 means failure, 0 means success
+    }
+}
 
 app.get('/', (req, res) => {
     res.send('Its working! 123');
@@ -11,8 +30,12 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5001;
 
-console.log("my port:",process.env.PORT);
+// app.listen(PORT, () => {
+//   console.log('Server is running on http://localhost:' + PORT);
+// })
 
-app.listen(PORT, () => {
-  console.log('Server is running on http://localhost:' + PORT);
+initDB().then(() => {
+    app.listen(PORT, () => {
+        console.log('Server is running on http://localhost:' + PORT);
+    });
 })
