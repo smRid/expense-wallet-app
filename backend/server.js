eba105ef04a3b8e960771bca932fc03835e2090a
smRid <omorsk911@gmail.com>
2025-05-31 15:28:44 +0600

    create transaction route added

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,15 @@ dotenv.config();
 
 const app = express();
 
-async function initDB() {
+app.use(express.json()); // Middleware to parse JSON bodies
+
+// custom middleware 
+// app.use((req, res, next) => {
+//     console.log("Hey we hit a request, the method is", req.method);
+//     next()
+// })
+
+async function initDB()  {
     try {
        await sql`CREATE TABLE IF NOT EXISTS transactions(
             id SERIAL PRIMARY KEY,
@@ -24,15 +32,37 @@ async function initDB() {
     }
 }
 
-app.get('/', (req, res) => {
-    res.send('Its working! 123');
+app.get("/", (req, res) => {
+    res.send("Its working!");
 });
+
 
 const PORT = process.env.PORT || 5001;
 
-// app.listen(PORT, () => {
-//   console.log('Server is running on http://localhost:' + PORT);
-// })
+
+app.post("/api/transactions", async (req, res) => {
+    //title, amount, category, user_id
+    try {
+       const { title, amount, category, user_id } = req.body;
+
+       if ( !title || !user_id || !category || amount === undefined ) {
+            return res.status(400).json({ message: "All fields are required" });
+       }
+
+    const transaction = await sql`
+        INSERT INTO transactions(user_id, title, amount, category)
+        VALUES (${user_id}, ${title}, ${amount}, ${category})
+        RETURNING *
+       `;
+
+       console.log(transaction);
+       res.status(201).json(transaction[0]); // Return the created transaction
+            
+    } catch (error) {
+        console.log('Error creating transaction', error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
 
 initDB().then(() => {
     app.listen(PORT, () => {
