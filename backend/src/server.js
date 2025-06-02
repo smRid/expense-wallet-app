2839cf4545de917afda1296f58d2de16b260ed20
smRid <omorsk911@gmail.com>
2025-06-02 15:38:01 +0600

    add cron job for periodic GET requests and update transaction summary display

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,13 @@ import { initDB } from './config/db.js';
 import ratelimiter from './middleware/rateLimiter.js';
 
 import transactionsRoute from './routes/transactionsRoute.js';
+import job from './config/cron.js';
 
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'production') job.start();
 
 //Middleware 
 app.use(ratelimiter);
@@ -21,7 +24,9 @@ app.use(express.json()); // Middleware to parse JSON bodies
 
 const PORT = process.env.PORT || 5001;
 
-
+app.get("/api/health", (req, res) => {
+    res.status(200).json({ status: "OK", message: "API is running smoothly!" });
+});
 
 app.use('/api/transactions', transactionsRoute);
 
--- mobile/hooks/useTransactions.js
+++ mobile/hooks/useTransactions.js
@@ -1,7 +1,8 @@
 import { useCallback, useState } from "react";
 import { Alert } from "react-native";
 
-const API_URL = "http://10.0.2.2:5001/api";
+// const API_URL = "http://10.0.2.2:5001/api";
+const API_URL = "https://wallet-app-api-txbi.onrender.com/api";
 
 export const useTransactions = (userId) => {
     const [transactions, setTransactions] = useState([]);
